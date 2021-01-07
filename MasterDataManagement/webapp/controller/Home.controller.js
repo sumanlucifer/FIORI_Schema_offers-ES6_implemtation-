a772c5af2aa28385b93e68fa085734489b2faf93
manik saluja <manik.saluja@extentia.com>
2021-01-07 14:51:38 +0000

    code indentaion and formatting

--- MasterDataManagement/webapp/controller/Home.controller.js
+++ MasterDataManagement/webapp/controller/Home.controller.js
@@ -1,17 +1,18 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-	/**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("com.knpl.pragati.MasterDataManagement.controller.Home", {
-            onInit: function () {
-                //this.getOwnerComponent().getModel("tableData").setDefaultBindingMode("OneWay");
-
-            },
-
-        });
-    });
+    return Controller.extend(
+      "com.knpl.pragati.MasterDataManagement.controller.Home",
+      {
+        onInit: function () {
+          console.log("manik");
+        },
+      }
+    );
+  }
+);
