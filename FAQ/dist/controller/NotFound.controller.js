2003c67e8ffd375bc2ad07a22cb11e5b83f0e8ef
Debasisa Pradhan <debasisa.pradhan@extentia.com>
2021-01-14 12:57:10 +0000

    FAQ renamed as Manage_FAQ

--- FAQ/dist/controller/NotFound.controller.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["./BaseController"],function(n){"use strict";return n.extend("com.knpl.pragati.FAQ.controller.NotFound",{onLinkPressed:function(){this.getRouter().navTo("worklist")}})});
