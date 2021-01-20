84d3aaa9f71a9add51c0ccf0224e3c686848750e
Venkatesh.Hulekal <Venkatesh.Hulekal@extentia.com>
2021-01-20 10:11:26 +0000

    Backend integrated with Dealer Details page

--- DealerManagement/webapp/controller/DealerDetails.controller.js
+++ DealerManagement/webapp/controller/DealerDetails.controller.js
@@ -12,7 +12,26 @@ sap.ui.define([
 
         return Controller.extend("com.knpl.pragati.DealerManagement.controller.DealerDetails", {
             onInit: function () {
-        
+                this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                this.oRouter.getRoute("RouteDetailsPage").attachPatternMatched(this._onObjectMatched, this);
+                this.KNPLModel = this.getOwnerComponent().getModel("KNPLModel");
+            },
+
+            _onObjectMatched: function (oEvent) {
+                var sObjectId = oEvent.getParameter("arguments").dealerID;
+                this.KNPLModel.metadataLoaded().then(function () {
+                    var sObjectPath = this.KNPLModel.createKey("PainterSet", {
+                        Id: sObjectId
+                    });
+                    this._bindView("/" + sObjectPath);
+                }.bind(this));
+            },
+
+            _bindView: function (sObjectPath) {
+                this.getView().bindElement({
+                    path: sObjectPath,
+                    model: "KNPLModel"
+                });
             }
         });
     });
