b6fe07e1a305993c5048b4097121e102332dec8c
himank.jain <himank.jain@extentia.com>
2021-03-24 16:17:40 +0000

    Remarks field is added while update status in callback request

--- CallbackRequests/webapp/controller/LandingPage.controller.js
+++ CallbackRequests/webapp/controller/LandingPage.controller.js
@@ -6,7 +6,15 @@ sap.ui.define(
     "sap/ui/model/FilterOperator",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageBox",
-    "sap/m/MessageToast",
+    "sap/m/Dialog",
+	"sap/m/DialogType",
+	"sap/m/Button",
+	"sap/m/ButtonType",
+	"sap/m/Label",
+	"sap/m/MessageToast",
+	"sap/m/Text",
+    "sap/m/TextArea",
+    "sap/ui/core/Core"
   ],
   function (
     BaseController,
@@ -14,7 +22,15 @@ sap.ui.define(
     FilterOperator,
     JSONModel,
     MessageBox,
-    MessageToast
+    Dialog,
+    DialogType,
+    Button,
+    ButtonType,
+    Label,
+    MessageToast,
+    Text,
+    TextArea,
+    Core
   ) {
     "use strict";
 
@@ -67,39 +83,61 @@ sap.ui.define(
             .getResourceBundle();
           var oPayload = {
             Status: "RESOLVED",
+            Remarks: ""
           };
-          MessageBox.confirm(
-            oResourceBundle.getText("updateConfirmationMessage"),
-            {
-              actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
-              emphasizedAction: MessageBox.Action.OK,
-              styleClass: "sapUiSizeCompact",
-              onClose: function (sAction) {
-                if (sAction == MessageBox.Action.OK) {
-                  oViewModel.setProperty("/busy", true);
-                  oModel.update(sPath, oPayload, {
-                    success: function () {
-                      MessageToast.show(
-                        oResourceBundle.getText(
-                          "messageBoxUpdateStatusSuccessMsg"
-                        )
-                      );
-                      oViewModel.setProperty("/busy", false);
-                      oView.byId("idPendingSmartTable").getModel().refresh();
-                    },
-                    error: function () {
-                      oViewModel.setProperty("/busy", false);
-                      MessageBox.error(
-                        oResourceBundle.getText(
-                          "messageBoxUpdateStatusErrorMsg-"
-                        )
-                      );
-                    },
-                  });
-                }
-              },
+          if (!this.oDialog) {
+				this.oDialog = new Dialog({
+					title: oResourceBundle.getText("confirmationDailogTitle"),
+					type: DialogType.Message,
+					content: [
+						new Label({
+							text: oResourceBundle.getText("updateConfirmationMessage"),
+							labelFor: "confirmNote"
+						}),
+						new TextArea("confirmNote", {
+                            width: "100%",
+                            maxLength: 512,
+							placeholder: oResourceBundle.getText("remarkInputPlaceholder")
+						})
+					],
+					beginButton: new Button({
+						type: ButtonType.Emphasized,
+						text: oResourceBundle.getText("confirmationDailogOkBtnText"),
+						press: function () {
+                            oPayload.Remarks = Core.byId("confirmNote").getValue();
+							oViewModel.setProperty("/busy", true);
+                            oModel.update(sPath, oPayload, {
+                                success: function () {
+                                    MessageToast.show(
+                                        oResourceBundle.getText(
+                                        "messageBoxUpdateStatusSuccessMsg"
+                                        )
+                                    );
+                                    oViewModel.setProperty("/busy", false);
+                                    oView.byId("idPendingSmartTable").getModel().refresh();
+                                },
+                                error: function () {
+                                    oViewModel.setProperty("/busy", false);
+                                    MessageBox.error(
+                                        oResourceBundle.getText(
+                                        "messageBoxUpdateStatusErrorMsg-"
+                                        )
+                                    );
+                                }
+                            });
+							this.oDialog.close();
+						}.bind(this)
+					}),
+					endButton: new Button({
+						text: oResourceBundle.getText("confirmationDailogCancelBtnText"),
+						press: function () {
+							this.oDialog.close();
+						}.bind(this)
+					})
+				});
             }
-          );
+            Core.byId("confirmNote").setValue("");
+			this.oDialog.open();
         },
       }
     );
