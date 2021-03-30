62cf6fd21996a2a1806b8ae5bd2837cf7b28939d
Debasisa Pradhan <debasisa.pradhan@extentia.com>
2021-03-30 12:38:52 +0000

    Added painter Type and Bug fix of training

--- Training_Learning/webapp/controller/TrainingTab.controller.js
+++ Training_Learning/webapp/controller/TrainingTab.controller.js
@@ -177,6 +177,108 @@ sap.ui.define(
                     that.getView().getModel().resetChanges();
                 },
 
+                onTablesSearch: function (oEvent) {
+                    var oView = this.getView();
+                    var sPath = oEvent.getSource().getBinding("value").getPath();
+                    var sValue = oEvent.getSource().getValue();
+                    var sTrainingId = oView
+                        .getModel("oModelControl2")
+                        .getProperty("/trainingId");
+                    console.log(sTrainingId);
+                    if (sPath.match("Attendance")) {
+                        // this._SearchAttendance(sValue, sTrainingId);
+                    } else if (sPath.match("Enrollment")) {
+                        // this._SearchEnrollment(sValue, sTrainingId);
+                    }
+                },
+
+                _SearchAttendance: function (sValue, sTrainingId) {
+                    debugger;
+                    var oView = this.getView();
+                    var aCurrentFilter = [];
+
+                    var oTable = oView.byId("idTblAttendance");
+                    if (/^\+?(0|[1-9]\d*)$/.test(sValue)) {
+                        aCurrentFilter.push(
+                            new Filter(
+                                [
+                                    new Filter(
+                                        "MembershipCard",
+                                        FilterOperator.EQ,
+                                        sValue.trim().substring(0, 8)
+                                    ),
+                                    new Filter(
+                                        "Mobile",
+                                        FilterOperator.EQ,
+                                        sValue.trim().substring(0, 8)
+                                    ),
+                                ],
+                                false
+                            )
+                        );
+                    } else {
+                        aCurrentFilter.push(
+                            new Filter(
+                                "tolower(Name)",
+                                FilterOperator.Contains,
+                                "'" + sValue.trim().toLowerCase().replace("'", "''") + "'"
+                            )
+                        );
+                    }
+                    aCurrentFilter.push(
+                        new Filter("TrainingId", FilterOperator.EQ, parseInt(sTrainingId))
+                    );
+                    var endFilter = new Filter({
+                        filters: aCurrentFilter,
+                        and: true,
+                    });
+
+                    oTable.getBinding("items").filter(endFilter);
+                },
+
+                _SearchEnrollment: function (sValue, sTrainingId) {
+                    var oView = this.getView();
+                    var aCurrentFilter = [];
+
+                    var oTable = oView.byId("idTblEnrollment");
+                    if (/^\+?(0|[1-9]\d*)$/.test(sValue)) {
+                        aCurrentFilter.push(
+                            new Filter(
+                                [
+                                    new Filter(
+                                        "MembershipCard",
+                                        FilterOperator.EQ,
+                                        sValue.trim().substring(0, 8)
+                                    ),
+                                    new Filter(
+                                        "Mobile",
+                                        FilterOperator.EQ,
+                                        sValue.trim().substring(0, 8)
+                                    ),
+                                ],
+                                false
+                            )
+                        );
+                    } else {
+                        aCurrentFilter.push(
+                            new Filter(
+                                "tolower(Name)",
+                                FilterOperator.Contains,
+                                "'" + sValue.trim().toLowerCase().replace("'", "''") + "'"
+                            )
+                        );
+                    }
+                    aCurrentFilter.push(
+                        new Filter("TrainingId", FilterOperator.EQ, parseInt(sTrainingId))
+                    );
+                    var endFilter = new Filter({
+                        filters: aCurrentFilter,
+                        and: true,
+                    });
+
+                    oTable.getBinding("items").filter(endFilter);
+                },
+
                 setInitCity: function (sStateId) {
                     this._fnbusyItems({
                         getId: function () {
@@ -551,22 +653,22 @@ sap.ui.define(
                                                 target: "/TrainingDetails/RewardPoints"
                                             });
                                         } else
-                                        if (data.Url === "") {
-                                            oReturn.IsNotValid = true;
-                                            oReturn.sMsg.push("MSG_PLS_ENTER_ERR_URL");
-                                            aCtrlMessage.push({
-                                                message: "MSG_PLS_ENTER_ERR_URL",
-                                                target: "/TrainingDetails/Url"
-                                            });
-                                        } else
-                                            if (data.Url !== "" && !url.match(regex)) {
+                                            if (data.Url === "") {
                                                 oReturn.IsNotValid = true;
-                                                oReturn.sMsg.push("MSG_VALDTN_ERR_URL");
+                                                oReturn.sMsg.push("MSG_PLS_ENTER_ERR_URL");
                                                 aCtrlMessage.push({
-                                                    message: "MSG_VALDTN_ERR_URL",
+                                                    message: "MSG_PLS_ENTER_ERR_URL",
                                                     target: "/TrainingDetails/Url"
                                                 });
-                                            }
+                                            } else
+                                                if (data.Url !== "" && !url.match(regex)) {
+                                                    oReturn.IsNotValid = true;
+                                                    oReturn.sMsg.push("MSG_VALDTN_ERR_URL");
+                                                    aCtrlMessage.push({
+                                                        message: "MSG_VALDTN_ERR_URL",
+                                                        target: "/TrainingDetails/Url"
+                                                    });
+                                                }
 
                     if (aCtrlMessage.length) this._genCtrlMessages(aCtrlMessage);
                     return oReturn;
