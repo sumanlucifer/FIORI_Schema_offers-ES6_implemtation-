8df7eed7f05dcf47fef2a5e78c76f572394e0f3b
Manik Saluja <manik.saluja@extentia.com>
2021-04-13 10:49:54 +0000

    View offers changes in the details page

--- SchemeOffers/webapp/controller/DetailPage.controller.js
+++ SchemeOffers/webapp/controller/DetailPage.controller.js
@@ -1,47 +1,147 @@
 // @ts-ignore
-sap.ui.define([
+sap.ui.define(
+  [
     "com/knpl/pragati/SchemeOffers/controller/BaseController",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/model/json/JSONModel",
-    'sap/ui/model/Sorter',
-    'sap/ui/core/Fragment',
-    'sap/ui/Device',
+    "sap/ui/model/Sorter",
+    "sap/ui/core/Fragment",
+    "sap/ui/Device",
     "sap/m/MessageBox",
     "sap/m/MessageToast",
-    "sap/m/Avatar"
-],
-function (BaseController, Filter, FilterOperator, JSONModel, Sorter, Fragment, Device, MessageBox, MessageToast, Avatar) {
+    "sap/m/Avatar",
+    "../model/formatter",
+  ],
+  function (
+    BaseController,
+    Filter,
+    FilterOperator,
+    JSONModel,
+    Sorter,
+    Fragment,
+    Device,
+    MessageBox,
+    MessageToast,
+    Avatar,
+    formatter
+  ) {
     "use strict";
 
-    return BaseController.extend("com.knpl.pragati.SchemeOffers.controller.DetailPage", {
+    return BaseController.extend(
+      "com.knpl.pragati.SchemeOffers.controller.DetailPage",
+      {
+        formatter: formatter,
         onInit: function () {
-            //Router Object
-            var oViewModel = new JSONModel({
-                busy: false,
-                editable: false
-            });
-            this.getView().setModel(oViewModel, "DetailViewModel");
-            this.oViewModel = this.getView().getModel("DetailViewModel");
-            this.oResourceBundle = this.getOwnerComponent().getModel('i18n').getResourceBundle();
-            this.oRouter = this.getRouter();
-            this.oRouter.getRoute("DetailPage").attachPatternMatched(this._onObjectMatched, this);
+          //Router Object
+          var oViewModel = new JSONModel({
+            busy: false,
+            editable: false,
+          });
+          this.getView().setModel(oViewModel, "DetailViewModel");
+          this.oViewModel = this.getView().getModel("DetailViewModel");
+          this.oResourceBundle = this.getOwnerComponent()
+            .getModel("i18n")
+            .getResourceBundle();
+          this.oRouter = this.getRouter();
+          this.oRouter
+            .getRoute("DetailPage")
+            .attachPatternMatched(this._onObjectMatched, this);
         },
 
         _onObjectMatched: function (oEvent) {
-            this._property = oEvent.getParameter("arguments").property;
+          var oProp = window.decodeURIComponent(
+            oEvent.getParameter("arguments").prop
+          );
+
+          var oView = this.getView();
+          var sExpandParam = "CreatedByDetails,SchemeType";
+          console.log(oProp);
+          if (oProp.trim() !== "") {
+            oView.bindElement({
+              path: "/" + oProp,
+              parameters: {
+                expand: sExpandParam,
+              },
+            });
+          }
+          this._initData(oProp);
+        },
+        _initData: function (oProp) {
+          var oData = {
+            modeEdit: true,
+            bindProp: oProp,
+
+            PainterId: oProp.replace(/[^0-9]/g, ""),
+            //ProfilePic:"/KNPL_PAINTER_API/api/v2/odata.svc/PainterSet(717)/$value",
+            tableDealay: 0,
+          };
+          var oModel = new JSONModel(oData);
+          this.getView().setModel(oModel, "oModelControl2");
+
+          var othat = this;
+          var c1, c2, c3, c4;
+          c1 = this._loadEditProfile("Display");
+          c1.then(function () {
+            c2 = othat._getInitData(oProp);
+            c2.then(function (data) {
+              c3 = othat._setViewData(data);
+            });
+          });
         },
 
         onPressEdit: function () {
-            this.oViewModel.setProperty("/editable", true);
+          this.oViewModel.setProperty("/editable", true);
         },
 
         onPressSave: function () {
-            this.oViewModel.setProperty("/editable", false);
+          this.oViewModel.setProperty("/editable", false);
         },
-
-        onPressCancel: function () {
-            this.oViewModel.setProperty("/editable", false);
-        }
-    });
-});
+        _getInitData: function () {
+          var promise = jQuery.Deferred();
+          var oView = this.getView();
+          var oData = oView.getModel();
+          var oModelControl2 = oView.getModel("oModelControl2");
+          var sPath = oModelControl2.getProperty("/bindProp");
+          var othat = this;
+          oView.getModel().read("/" + sPath, {
+            urlParameters: {
+              $expand: "SchemeZones,SchemeDepots",
+            },
+            success: function (data) {
+              promise.resolve(data);
+            },
+            error: function () {
+              promise.reject();
+            },
+          });
+          return promise;
+        },
+        _setViewData: function (oData) {
+          console.log(oData);
+          var oView = this.getView();
+          var oModelControl2 = oView.getModel("oModelControl2");
+        },
+        _initViewData: function () {},
+        _loadEditProfile: function (mParam) {
+          var oView = this.getView();
+          var promise = jQuery.Deferred();
+          var othat = this;
+          var oVboxProfile = oView.byId("idVbProfile");
+          var sFragName = mParam == "Edit" ? "EditOffer" : "DisplayDetail";
+          oVboxProfile.destroyItems();
+          return Fragment.load({
+            id: oView.getId(),
+            controller: othat,
+            name: "com.knpl.pragati.SchemeOffers.view.fragment." + sFragName,
+          }).then(function (oControlProfile) {
+            oView.addDependent(oControlProfile);
+            oVboxProfile.addItem(oControlProfile);
+            promise.resolve();
+            return promise;
+          });
+        },
+      }
+    );
+  }
+);
--- SchemeOffers/webapp/model/formatter.js
+++ SchemeOffers/webapp/model/formatter.js
@@ -1,22 +1,46 @@
 // @ts-ignore
-sap.ui.define([], function () {
-	"use strict";
+sap.ui.define(["sap/ui/core/format/DateFormat"], function (DateFormat) {
+  "use strict";
 
-	return {
+  return {
+    /**
+     * Rounds the number unit value to 2 digits
+     * @public
+     * @param {string} sValue the number string to be rounded
+     * @returns {string} sValue with 2 digits rounded
+     */
+    numberUnit: function (sValue) {
+      if (!sValue) {
+        return "";
+      }
+      return parseFloat(sValue).toFixed(2);
+    },
+    fmtDate: function (mDate) {
+      var date = new Date(mDate);
+      var oDateFormat = DateFormat.getDateTimeInstance({
+        pattern: "dd/MM/yyyy",
+        UTC: true,
+        strictParsing: true,
+      });
+      return oDateFormat.format(date);
+    },
+    fmtLowerCase: function (mParam) {
+      var sStatus = "";
+      if (mParam) {
+        sStatus = mParam;
+        sStatus = sStatus.toLowerCase();
+        var aCharStatus = sStatus.split("");
+        if (aCharStatus.indexOf("_") !== -1) {
+          aCharStatus[aCharStatus.indexOf("_") + 1] = aCharStatus[
+            aCharStatus.indexOf("_") + 1
+          ].toUpperCase();
+          aCharStatus.splice(aCharStatus.indexOf("_"), 1, " ");
+        }
+        aCharStatus[0] = aCharStatus[0].toUpperCase();
+        sStatus = aCharStatus.join("");
+      }
 
-		/**
-		 * Rounds the number unit value to 2 digits
-		 * @public
-		 * @param {string} sValue the number string to be rounded
-		 * @returns {string} sValue with 2 digits rounded
-		 */
-		numberUnit : function (sValue) {
-			if (!sValue) {
-				return "";
-			}
-			return parseFloat(sValue).toFixed(2);
-		}
-
-	};
-
+      return sStatus;
+    },
+  };
 });
