d4d7591506cfc575fb53a86739541ca5738566a9
adityashrirang.h <adityashrirang.h@extentia.com>
2021-04-13 06:51:24 +0000

    updated role access and label changes in catalogue

--- Catelogue/webapp/controller/App.controller.js
+++ Catelogue/webapp/controller/App.controller.js
@@ -1,35 +1,49 @@
 sap.ui.define([
-	"./BaseController",
-	"sap/ui/model/json/JSONModel"
+    "./BaseController",
+    "sap/ui/model/json/JSONModel"
 ], function (BaseController, JSONModel) {
-	"use strict";
+    "use strict";
 
-	return BaseController.extend("com.knpl.pragati.Catelogue.controller.App", {
+    return BaseController.extend("com.knpl.pragati.Catelogue.controller.App", {
 
-		onInit : function () {
-			var oViewModel,
-				fnSetAppNotBusy,
-				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+        onInit: function () {
+            var oViewModel,
+                fnSetAppNotBusy,
+                iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 
-			oViewModel = new JSONModel({
-				busy : true,
-				delay : 0
-			});
-			this.setModel(oViewModel, "appView");
+            oViewModel = new JSONModel({
+                busy: true,
+                delay: 0
+            });
+            this.setModel(oViewModel, "appView");
 
-			fnSetAppNotBusy = function() {
-				oViewModel.setProperty("/busy", false);
-				oViewModel.setProperty("/delay", iOriginalBusyDelay);
-			};
+            fnSetAppNotBusy = function () {
 
-			// disable busy indication when the metadata is loaded and in case of errors
-			this.getOwnerComponent().getModel().metadataLoaded().
-				then(fnSetAppNotBusy);
-			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
 
-			// apply content density mode to root view
-			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-		}
-	});
+                //Fetch loggedIn User ID to disable delete button for loggedIn user
+
+                var oModel = this.getOwnerComponent().getModel()
+                oModel.callFunction("/GetLoggedInAdmin", {
+                    method: "GET",
+                    success: function (data) {
+                        oViewModel.setProperty("/busy", false);
+                        oViewModel.setProperty("/delay", iOriginalBusyDelay);
+                        oViewModel.setProperty("/loggedUserId", data.results[0].Id);
+                        oViewModel.setProperty("/loggedUserRoleId", data.results[0].RoleId);
+                    }
+                });
+            }.bind(this);
+
+
+
+            // disable busy indication when the metadata is loaded and in case of errors
+            this.getOwnerComponent().getModel().metadataLoaded().
+                then(fnSetAppNotBusy);
+            this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
+
+            // apply content density mode to root view
+            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+        }
+    });
 
 });
