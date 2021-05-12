e1814c78aafe4668445bd6f9787024b166576f4f
Himank Jain <himank.jain@extentia.com>
2021-05-12 07:48:24 +0000

    bash: clean build files

--- Deploy.sh
+++ Deploy.sh
@@ -5,6 +5,7 @@ mbt build -s '/home/user/projects/knpl-influencer-web'
 if [[ $? -eq 0 ]]; then
     echo "build complete Now deploying"
     cf deploy mta_archives/knpl-influencer-web_1.0.1.mtar --delete-services
+    git reset --hard
 else
     echo "corrupt MTA file please check."
 fi    
