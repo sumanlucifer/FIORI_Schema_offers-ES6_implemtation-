59a39ef4695765102617edefa2f3730a7232cb7e
Himank Jain <himank.jain@extentia.com>
2021-05-12 07:32:31 +0000

    bash: add courrpt mta validation

--- Deploy.sh
+++ Deploy.sh
@@ -1,9 +1,11 @@
-
-
 echo "Building Project"
 
 mbt build -s '/home/user/projects/knpl-influencer-web'
 
-echo "build complete Now deploying"
+if [[ $? -eq 0 ]]; then
+    echo "build complete Now deploying"
+    cf deploy mta_archives/knpl-influencer-web_1.0.1.mtar --delete-services
+else
+    echo "corrupt MTA file please check."
+fi    
 
-cf deploy mta_archives/knpl-influencer-web_1.0.1.mtar --delete-services
