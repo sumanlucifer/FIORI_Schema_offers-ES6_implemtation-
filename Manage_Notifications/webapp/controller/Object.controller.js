72fdadd41bb11a2729bb2f17f59eb967c46a1e8f
adityashrirang.h <adityashrirang.h@extentia.com>
2021-05-14 10:49:03 +0000

    Notifications select multiple painters added

--- Manage_Notifications/webapp/controller/Object.controller.js
+++ Manage_Notifications/webapp/controller/Object.controller.js
@@ -2,8 +2,11 @@ sap.ui.define([
 	"./BaseController",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
-	"sap/ui/core/SeparatorItem"
-], function (BaseController, JSONModel, formatter, SeparatorItem) {
+    "sap/ui/core/SeparatorItem",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+
+], function (BaseController, JSONModel, formatter, SeparatorItem,Filter,FilterOperator) {
 	"use strict";
 
 	return BaseController.extend("com.knpl.pragati.Manage_Notifications.controller.Object", {
@@ -26,7 +29,8 @@ sap.ui.define([
 				oViewModel = new JSONModel({
 					busy: true,
 					IsLater: true,
-					delay: 0
+                    delay: 0
+
 				});
 
 			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
@@ -85,7 +89,8 @@ sap.ui.define([
 			if (viewchar === "X") {
 				that.getModel("objectView").setProperty("/sMode", "X");
 				TtlNotification = this.getView().getModel("i18n").getResourceBundle().getText("TtlViewNotification");
-				this.getModel("objectView").setProperty("/TtlNotification", TtlNotification);
+                this.getModel("objectView").setProperty("/TtlNotification", TtlNotification);
+                this.getModel("objectView").setProperty("/Painters", []);
 			} else {
 				that.getModel("objectView").setProperty("/sMode", "E");
 				TtlNotification = this.getView().getModel("i18n").getResourceBundle().getText("TtlEditNotification");
@@ -452,7 +457,167 @@ sap.ui.define([
 					});
 				}
 			});
-		}
+        },
+        
+                onValueHelpRequestedPainter: function () {
+                    this._oMultiInput = this.getView().byId("multiInputPainterAdd");
+                    this.oColModel = new JSONModel({
+                        cols: [
+                            {
+                                label: "Membership ID",
+                                template: "MembershipCard",
+                            },
+                            {
+                                label: "Name",
+                                template: "Name",
+                            },
+                            {
+                                label: "Mobile Number",
+                                template: "Mobile",
+                            },
+                            {
+                                label: "Zone",
+                                template: "ZoneId",
+                            },
+                            {
+                                label: "Division",
+                                template: "DivisionId",
+                            },
+                            {
+                                label: "Depot",
+                                template: "Depot/Depot",
+                            },
+                            {
+                                label: "Painter Type",
+                                template: "PainterType/PainterType",
+                            },
+                            {
+                                label: "Painter ArcheType",
+                                template: "ArcheType/ArcheType",
+                            }
+                        ],
+                    });
+
+                    var aCols = this.oColModel.getData().cols;
+                    var oFilter = new sap.ui.model.Filter("IsArchived", sap.ui.model.FilterOperator.EQ, false);
+
+                    this._oValueHelpDialog = sap.ui.xmlfragment(
+                        "com.knpl.pragati.Manage_Notifications.view.fragments.PainterValueHelp",
+                        this
+                    );
+                    this.getView().addDependent(this._oValueHelpDialog);
+
+                    this._oValueHelpDialog.getTableAsync().then(
+                        function (oTable) {
+                            oTable.setModel(this.oColModel, "columns");
+
+                            if (oTable.bindRows) {
+                                oTable.bindAggregation("rows", {
+                                    path: "/PainterSet", filters: [oFilter], parameters: { expand: "Depot,PainterType,ArcheType" }, events:
+                                    {
+                                        dataReceived: function () {
+                                            this._oValueHelpDialog.update();
+                                        }.bind(this)
+                                    }
+                                });
+                            }
+
+                            if (oTable.bindItems) {
+                                oTable.bindAggregation("items", "/PainterSet", function () {
+                                    return new sap.m.ColumnListItem({
+                                        cells: aCols.map(function (column) {
+                                            return new sap.m.Label({
+                                                text: "{" + column.template + "}",
+                                            });
+                                        }),
+                                    });
+                                });
+                            }
+
+                            this._oValueHelpDialog.update();
+                        }.bind(this)
+                    );
+
+                    this._oValueHelpDialog.setTokens(this._oMultiInput.getTokens());
+                    this._oValueHelpDialog.open();
+                },
+
+                onFilterBarSearchPainter: function (oEvent) {
+                    //   debugger;
+                    var afilterBar = oEvent.getParameter("selectionSet"),
+                        aFilters = [];
+
+                    for (var i = 0; i < afilterBar.length; i++) {
+                        if (afilterBar[i].getValue()) {
+                            aFilters.push(
+                                new Filter({
+                                    path: afilterBar[i].mProperties.name,
+                                    operator: FilterOperator.Contains,
+                                    value1: afilterBar[i].getValue(),
+                                    caseSensitive: false,
+                                })
+                            );
+                        }
+                    }
+
+                    aFilters.push(
+                        new Filter({
+                            path: "IsArchived",
+                            operator: FilterOperator.EQ,
+                            value1: false,
+                        })
+                    );
+
+                    this._filterTable(
+                        new Filter({
+                            filters: aFilters,
+                            and: true,
+                        })
+                    );
+                },
+
+                onValueHelpCancelPressPainter: function () {
+                    this._oValueHelpDialog.close();
+                },
+
+                onValueHelpOkPressPainter: function (oEvent) {
+                    var oData = [];
+                    var xUnique = new Set();
+                    var aTokens = oEvent.getParameter("tokens");
+
+                    aTokens.forEach(function (ele) {
+                        if (xUnique.has(ele.getKey()) == false) {
+                            oData.push({
+                                Name: ele.getText(),
+                                PainterId: ele.getKey(),
+                                Id: ele.getKey()
+                            });
+                            xUnique.add(ele.getKey());
+                        }
+                    });
+
+                    this.getView().getModel("objectView").setProperty("/Painters", oData);
+                    this._oValueHelpDialog.close();
+                },
+                _filterTable: function (oFilter, sType) {
+                    var oValueHelpDialog = this._oValueHelpDialog;
+
+                    oValueHelpDialog.getTableAsync().then(function (oTable) {
+                        if (oTable.bindRows) {
+                            oTable.getBinding("rows").filter(oFilter, sType || "Application");
+                        }
+
+                        if (oTable.bindItems) {
+                            oTable
+                                .getBinding("items")
+                                .filter(oFilter, sType || "Application");
+                        }
+
+                        oValueHelpDialog.update();
+                    });
+                },
+
+
 
 	});
 
