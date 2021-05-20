1a667dacded7dca0d4168072082bed924a9621eb
Manik Saluja <manik.saluja@extentia.com>
2021-05-20 06:58:29 +0000

    parent offer dialog id

--- SchemeOffers/webapp/controller/DetailPage.controller.js
+++ SchemeOffers/webapp/controller/DetailPage.controller.js
@@ -1289,7 +1289,7 @@ sap.ui.define(
                     c6.then(function (oPayLoad) {
                       c7 = othat._UploadFile(oPayLoad, bFileFlag);
                       c7.then(function (data) {
-                        othat.handleCancelPress(data);
+                        //othat.handleCancelPress(data);
                       });
                     });
                   });
