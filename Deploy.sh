0a7808ff7148a27deef7adf0fc04ac8833ece3e0
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-03-28 09:33:09 +0000

    deploy.sh change

--- Deploy.sh
+++ Deploy.sh
@@ -1,9 +1,23 @@
 #Check whether target space is passed or not
-SPACE="PRD"
-VER=mta_archives/knpl-influencer-web_3.1.1.mtar
+case $1 in
+  D)
+    SPACE="DEV"
+    VER=mta_archives/knpl-influencer-web_1.0.1.mtar
+    ;;
+
+  U)
+    SPACE="UAT"
+    VER=mta_archives/knpl-influencer-web_1.0.0.mtar
+    ;;
+
+  *)
+   echo "Ileagal selected space. Please send D for DEV or U for UAT as argument"
+   exit 1
+    ;;  
+esac
 
 #Login to CF space
-cf login -a https://api.cf.ap11.hana.ondemand.com -u ankit.pundhir@extentia.com -p Admin@2022 -o "Kansai Nerolac Paints Ltd_knpl-painter-prd" -s "${SPACE}"
+cf login -a https://api.cf.ap10.hana.ondemand.com -u ankit.pundhir@extentia.com -p Admin@2022 -o "Kansai Nerolac Paints Ltd_knpl-painter-dev" -s "${SPACE}"
 
 echo "Building Project"
 mbt build -s '/home/user/projects/knpl-influencer-web'
