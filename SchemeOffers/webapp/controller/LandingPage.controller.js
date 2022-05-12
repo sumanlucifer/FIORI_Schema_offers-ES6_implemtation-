411790a601004fe36e8a3c0c8909b9596ea9b86f
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-05-12 18:16:34 +0000

    new change for offer lead filters

--- SchemeOffers/webapp/controller/LandingPage.controller.js
+++ SchemeOffers/webapp/controller/LandingPage.controller.js
@@ -87,26 +87,76 @@ sap.ui.define(
                         oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
                     });
                     this.getView().getModel().refresh();
-                    this._getLoggedInInfo();
-
+                    var c1,c2,c3;
+                    var othat=this;
+                    c1= othat._getLoggedInInfo();
+                    c1.then(function(){
+                        c2=othat._initLoginFilterTable1()
+                    })
                 },
                 _getLoggedInInfo: function () {
                     var oData = this.getView().getModel();
                     var oLoginData = this.getView().getModel("LoginInfo");
-                    oData.callFunction("/GetLoggedInAdmin", {
-                        method: "GET",
-                        urlParameters: {
-                            $expand: "UserType",
-                        },
-                        success: function (data) {
-                            if (data.hasOwnProperty("results")) {
-                                if (data["results"].length > 0) {
-                                    oLoginData.setData(data["results"][0]);
-                                    //console.log(oLoginData)
+                    return new Promise((resolve,reject)=>{
+                        oData.callFunction("/GetLoggedInAdmin", {
+                            method: "GET",
+                            urlParameters: {
+                                $expand: "UserType,AdminZone,AdminDivision",
+                            },
+                            success: function (data) {
+                                if (data.hasOwnProperty("results")) {
+                                    if (data["results"].length > 0) {
+                                        oLoginData.setData(data["results"][0]);
+                                        //console.log(oLoginData)
+                                    }
                                 }
+                                resolve();
+                            },
+                        });
+                    })
+                },
+                _initLoginFilterTable1:function(){
+                    var promise = $.Deferred();
+                    var oView = this.getView();
+                    var oLoginData = this.getView().getModel("LoginInfo").getData();
+                    var aFilter = [];
+                    var aLeadsFilter = this._CreateLeadsFilter()
+                    if (aLeadsFilter) {
+                        oView.byId("idOffersTable").getBinding("items").filter(new Filter({
+                            filters: aLeadsFilter,
+                            and: true
+                        }), "Application")
+                    }
+
+
+                    promise.resolve();
+                    return promise;
+                },
+                _CreateLeadsFilter: function (mParam1) {
+                    var oView = this.getView();
+                    var oLoginData = oView.getModel("LoginInfo").getData();
+                    var aFilter = [];
+                    if (oLoginData["UserTypeId"] === 3) {
+                        if (oLoginData["AdminDivision"]["results"].length > 0) {
+                            for (var x of oLoginData["AdminDivision"]["results"]) {
+                                aFilter.push(new Filter("OfferDivision/DivisionId", FilterOperator.EQ, x["DivisionId"]))
                             }
-                        },
-                    });
+                        }else if (oLoginData["AdminZone"]["results"].length > 0) {
+                            for (var x of oLoginData["AdminZone"]["results"]) {
+                                aFilter.push(new Filter("OfferZone/ZoneId", FilterOperator.EQ, x["ZoneId"]))
+                            }
+                        }
+                        if (aFilter.length > 0) {
+                            var aEndFilter = [new Filter("IsArchived", FilterOperator.EQ,false)];
+                            aEndFilter.push(new Filter({
+                                filters: aFilter,
+                                and: false
+                            }))
+                            return aEndFilter;
+
+                        }
+                    }
+                    return false;
                 },
 
 
--- SchemeOffers/webapp/model/formatter.js
+++ SchemeOffers/webapp/model/formatter.js
@@ -524,6 +524,46 @@ sap.ui.define(["sap/ui/core/format/DateFormat"], function (DateFormat) {
 
             }
 
+        },
+        fmtLeadZoneCheck: function (mParam1, mParam2, mParam3) {
+            //console.log(mParam1, mParam2, mParam3);
+            if (mParam2 !== 3) {
+                return true
+            }
+            if (mParam3) {
+                if(mParam3.hasOwnProperty("results")){
+                    if (mParam3["results"].length > 0) {
+                        for (var x of mParam3["results"]) {
+                            if (x["ZoneId"] == mParam1) {
+                                return true;
+    
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        },
+        fmtLeadDivisionCheck: function (mParam1, mParam2, mParam3) {
+            //console.log(mParam1, mParam2, mParam3);
+            if (mParam2 !== 3) {
+                return true
+            }
+            if (mParam3) {
+                if(mParam3.hasOwnProperty("results")){
+                    if (mParam3["results"].length > 0) {
+                        for (var x of mParam3["results"]) {
+                            if (x["DivisionId"] == mParam1) {
+                                return true;
+    
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
     };
