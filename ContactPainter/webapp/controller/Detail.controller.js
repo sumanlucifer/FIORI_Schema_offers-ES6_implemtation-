e5994626c7038866f853812adda67e45e8fa8fcc
Mamta Singh <mamta.s@extentia.com>
2022-09-20 05:20:34 +0000

    binding remove

--- ContactPainter/webapp/controller/Detail.controller.js
+++ ContactPainter/webapp/controller/Detail.controller.js
@@ -203,7 +203,7 @@ sap.ui.define([
                     var DGAModel = this.getOwnerComponent().getModel("DGAModel");
                     this.getView().setModel(DGAModel);
                     this.getView().bindElement({
-                        path: "DGAModel>/" + oProp,
+                        path: "/" + oProp,
                         parameters: {
                             expand: exPand,
                         },
@@ -229,8 +229,8 @@ sap.ui.define([
                 var sKey = oEvent.getSource().getSelectedKey();
                 var oView = this.getView();
                 if (sKey == "1") {
-                    var DGAModel = oView.getModel("DGAModel");
-                    oView.byId("QuotationTbl1").setModel(DGAModel);
+                    // var DGAModel = oView.getModel("DGAModel");
+                    // oView.byId("QuotationTbl1").setModel(DGAModel);
                     oView.byId("QuotationTbl1").rebindTable();
                     oView.byId("QuotationTbl2").rebindTable();
                     oView.byId("QuotationTbl3").rebindTable();
@@ -291,78 +291,7 @@ sap.ui.define([
                 return promise;
             },
 
-            // _bindMRTbl: function (oEvent, iPaintingReqId) {
-            //     var promise = jQuery.Deferred();
-            //     var oView = this.getView();
-            //     var sId = oView.getModel("oModelDisplay").getProperty("/Id");
-            //     var oBindingParams = oEvent.getParameter("bindingParams");
-            //     oBindingParams.parameters["expand"] = "Product,ProductShade,Product/ProductClassification";
-            //     var oFiler = new Filter("LeadId", FilterOperator.EQ, sId);
-            //     var oPaintingReqIdFiler = new Filter("PaintingReqId", FilterOperator.EQ, iPaintingReqId);
-            //     oBindingParams.filters.push(oFiler, oPaintingReqIdFiler);
-            //     // oBindingParams.sorter.push(new Sorter("CreatedAt", true));
-            //     promise.resolve();
-            //     return promise;
-            // },
-
-            // before binding methods of the smart tables
-            // onBeforeRebindPreReq1: function (oEvent) {
-            //     var oView = this.getView();
-            //     var c1 = this._bindPreEstimationTbl(oEvent, 1);
-            //     var othat = this;
-            //     c1.then(() => {
-            //         var oBindingObject = oEvent.getSource().getBindingContext().getObject();
-            //         var sPreEstimationPath = oBindingObject.PreEstimation.__list[0];
-            //         this._bindViewElement("idTotalInterior", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idLblTotalInterior", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idPreEstGTotal", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idPreEstimationDate", "/" + sPreEstimationPath);
-            //     });
-            // },
-            // onBeforeRebindPreReq2: function (oEvent) {
-            //     var oView = this.getView();
-            //     var c1 = this._bindPreEstimationTbl(oEvent, 2);
-            //     var othat = this;
-            //     c1.then(() => {
-            //         var oBindingObject = oEvent.getSource().getBindingContext().getObject();
-            //         var sPreEstimationPath = oBindingObject.PreEstimation.__list[0];
-            //         this._bindViewElement("idTotalExterior", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idLblTotalExterior", "/" + sPreEstimationPath);
-            //     });
-            // },
-            // onBeforeRebindPreReq3: function (oEvent) {
-            //     var oView = this.getView();
-            //     var c1 = this._bindPreEstimationTbl(oEvent, 3);
-            //     var othat = this;
-            //     c1.then(() => {
-            //         var oBindingObject = oEvent.getSource().getBindingContext().getObject();
-            //         var sPreEstimationPath = oBindingObject.PreEstimation.__list[0];
-            //         this._bindViewElement("idTotalWC", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idLblTotalWC", "/" + sPreEstimationPath);
-            //     });
-            // },
-            // onBeforeRebindPreReq4: function (oEvent) {
-            //     var oView = this.getView();
-            //     var c1 = this._bindPreEstimationTbl(oEvent, 4);
-            //     var othat = this;
-            //     c1.then(() => {
-            //         var oBindingObject = oEvent.getSource().getBindingContext().getObject();
-            //         var sPreEstimationPath = oBindingObject.PreEstimation.__list[0];
-            //         this._bindViewElement("idTotalCC", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idLblTotalCC", "/" + sPreEstimationPath);
-            //     });
-            // },
-            // onBeforeRebindPreReq5: function (oEvent) {
-            //     var oView = this.getView();
-            //     var c1 = this._bindPreEstimationTbl(oEvent, 5);
-            //     var othat = this;
-            //     c1.then(() => {
-            //         var oBindingObject = oEvent.getSource().getBindingContext().getObject();
-            //         var sPreEstimationPath = oBindingObject.PreEstimation.__list[0];
-            //         this._bindViewElement("idTotalEnamel", "/" + sPreEstimationPath);
-            //         this._bindViewElement("idLblTotalEnamel", "/" + sPreEstimationPath);
-            //     });
-            // },
+          
             onBeforeRebindQuotReq1: function (oEvent) {
                 var oView = this.getView();
                 var c1 = this._bindQuotationTbl(oEvent, 1);
