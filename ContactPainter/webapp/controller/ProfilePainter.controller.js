aade505cc73d4a140188cb8bd6e5ac7c44311f54
Mamta Singh <mamta.s@extentia.com>
2022-09-26 11:16:29 +0000

    filter added

--- ContactPainter/webapp/controller/ProfilePainter.controller.js
+++ ContactPainter/webapp/controller/ProfilePainter.controller.js
@@ -4617,8 +4617,7 @@ sap.ui.define(
                 var oBindingParams = oEvent.getParameter("bindingParams");
                 oBindingParams.parameters["expand"] = "DGA, DGADetails, LeadServiceType, State,AssignedContractors,LeadStatus, Depot, PaintingReqSlab, LeadSource,Dealer";
                 var oFilter1 = new Filter("ContractorId", FilterOperator.EQ, oDGAModel);
-                oBindingParams.filter([oFilter1]);
-                
+                oBindingParams.filters.push(oFilter1);
                 oBindingParams.sorter.push(new Sorter("CreatedAt", true));
 
             },
