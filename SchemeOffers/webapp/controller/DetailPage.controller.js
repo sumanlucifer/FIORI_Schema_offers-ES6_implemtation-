c914a6ed479ff568fb6e3794df964a4de1b2b888
Deepanjali Kumari <Deepanjali.Kumari@extentia.com>
2022-10-06 06:40:15 +0000

    new changes

--- SchemeOffers/webapp/controller/DetailPage.controller.js
+++ SchemeOffers/webapp/controller/DetailPage.controller.js
@@ -534,18 +534,18 @@ sap.ui.define(
                     Table2 = [];
 
                 // added by deepanjali strat
-                if (oData["BannerMediaList"]["results"].length > 0) {
-                    oModelControl2.setProperty(
-                        "/Table/Table11",
-                        oData["BannerMediaList"]["results"]
-                    );
-                }
-                if (oData["PamphletMediaList"]["results"].length > 0) {
-                    oModelControl2.setProperty(
-                        "/Table/Table12",
-                        oData["PamphletMediaList"]["results"]
-                    );
-                }
+                // if (oData["BannerMediaList"]["results"].length > 0) {
+                //     oModelControl2.setProperty(
+                //         "/Table/Table11",
+                //         oData["BannerMediaList"]["results"]
+                //     );
+                // }
+                // if (oData["PamphletMediaList"]["results"].length > 0) {
+                //     oModelControl2.setProperty(
+                //         "/Table/Table12",
+                //         oData["PamphletMediaList"]["results"]
+                //     );
+                // }
                 // added by deepanjali end
                 if (oData["OfferRewardRatio"]["results"].length > 0) {
                     oModelControl2.setProperty(
@@ -1463,18 +1463,18 @@ sap.ui.define(
                     );
                 }
                 /// commented by deepanjali start//
-                if (oData["BannerMediaList"]["results"].length > 0) {
-                    oModelControl2.setProperty(
-                        "/Table/Table11",
-                        oData["BannerMediaList"]["results"]
-                    );
-                }
-                if (oData["PamphletMediaList"]["results"].length > 0) {
-                    oModelControl2.setProperty(
-                        "/Table/Table12",
-                        oData["PamphletMediaList"]["results"]
-                    );
-                }
+                // if (oData["BannerMediaList"]["results"].length > 0) {
+                //     oModelControl2.setProperty(
+                //         "/Table/Table11",
+                //         oData["BannerMediaList"]["results"]
+                //     );
+                // }
+                // if (oData["PamphletMediaList"]["results"].length > 0) {
+                //     oModelControl2.setProperty(
+                //         "/Table/Table12",
+                //         oData["PamphletMediaList"]["results"]
+                //     );
+                // }
                 /// commented by deepanjali end//
                 // if (oData["IsSpecificApplicablePack"] === false) {
                 //     if (oData["OfferRewardRatio"]["results"].length > 0) {
